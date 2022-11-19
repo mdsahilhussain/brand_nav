0a6fa1f56d8f3265ce5f60b2f2adf7cf4d2a5f30
mdsahilhussain <mdsahil.hussain7@gmail.com>
2022-11-19 17:54:04 +0530

    add assests and folder of componets

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Button/Button";
+export { default as About } from "./About/About";
+export { default as Features } from "./Features/Features";
+export { default as Pricing } from "./Pricing/Pricing";
+export { default as Testimonials } from "./Testimonials/Testimonials";
+export { default as Help } from "./Help/Help";
+export { default as Navbar } from "./Navbar/Navbar";
