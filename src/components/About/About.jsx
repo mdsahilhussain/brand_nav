7b01d4904ad3e5c60cf84d2bc864c3674faf6478
mdsahilhussain <mdsahil.hussain7@gmail.com>
2022-11-20 11:49:38 +0530

    update about and features

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
+import "./about.css";
+import addressIcon from "../../assets/Group 1215.svg";
+import userIcon from "../../assets/Group 1216.svg";
+import dataBaseIcon from "../../assets/Group 1217.svg";
+
+const aboutInfo = [
+  {
+    iconImage: userIcon,
+    titel: "90+",
+    subTitel: "Users",
+  },
+  {
+    iconImage: addressIcon,
+    titel: "30+",
+    subTitel: "Locations",
+  },
+  {
+    iconImage: dataBaseIcon,
+    titel: "50+",
+    subTitel: "Servers",
+  },
+];
+
 function About() {
-  return <div>About</div>;
+  return (
+    <section className="about">
+      {aboutInfo.map((item, index) => (
+        <div key={index} className="about___card">
+          <div className="about___card--image">
+            <img src={item.iconImage} alt="" />
+          </div>
+          <div className="about___card--detail">
+            <h5>{item.titel}</h5>
+            <p>{item.subTitel}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default About;
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import "./features.css";
+
+import featuresImg from "../../assets/Illustration 2.svg";
+import pointsImg from "../../assets/Group 1120.svg";
+
+const featuresInfo = [
+  {
+    featuresPoint: "Powerfull online protection.",
+  },
+  {
+    featuresPoint: "Internet without borders.",
+  },
+  {
+    featuresPoint: "Supercharged VPN",
+  },
+  {
+    featuresPoint: "No specific time limits.",
+  },
+];
 
 function Features() {
   return (
-    <div>Features</div>
-  )
+    <section className="features">
+      <img src={featuresImg} alt="featuresImg" />
+      <div className="features__detail">
+        <h2>We Provide Many Features You Can Use</h2>
+        <p>
+          You can explore the features that we provide with fun and have their
+          own functions each feature.
+        </p>
+        {featuresInfo.map((item, index) => (
+          <p key={index}>
+            <img src={pointsImg} alt="pointsImg"></img>
+            {item.featuresPoint}
+          </p>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Features
+export default Features;
