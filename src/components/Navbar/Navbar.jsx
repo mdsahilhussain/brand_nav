8b37d968a0c8937086aef8901e9a4144b4e9a6c9
mdsahilhussain <mdsahil.hussain7@gmail.com>
2022-11-21 15:18:49 +0530

    add responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { Button } from "../index";
 
 import Logo from "../../assets/Logo.svg";
+import menuIcon from "../../assets/menu.png";
+import closeIcon from "../../assets/close.png";
 
 const navInfo = [
   { navName: "About", url: "#about" },
@@ -13,24 +15,69 @@ const navInfo = [
 ];
 
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className="nav container">
-      <ui className="nav___image">
-        <img src={Logo} alt="logoIMG" />
-      </ui>
-      <ui className="nav___links">
-        {navInfo.map((item, key) => (
-          <li>
-            <a href={item.url}>
-              <p>{item.navName}</p>
-            </a>
-          </li>
-        ))}
-      </ui>
-      <ui className="nav___button">
-        <Button name="Sign In" type="btn___normal" />
-        <Button name="Sing up" type="btn___border" />
-      </ui>
+      <div className="menubar">
+        <div className="menubar___style">
+          <ui className="menubar___image">
+            <img src={Logo} alt="logoIMG" />
+          </ui>
+          {toggleMenu ? (
+            <img
+              src={closeIcon}
+              alt="closeIcon"
+              className="menubar___image"
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <img
+              src={menuIcon}
+              alt="menuIcon"
+              className="menubar___image"
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+        </div>
+        {toggleMenu && (
+          <div className="menubar--linkList">
+            <ul>
+              {navInfo.map((item, index) => (
+                <li key={index}>
+                  <a href={item.url}>
+                    <p>{item.navName}</p>
+                  </a>
+                </li>
+              ))}
+              <div className=".menubar___button">
+                <Button name="Sign In" type="btn___normal" /> <br />
+                <Button name="Sing up" type="btn___border" />
+              </div>
+            </ul>
+          </div>
+        )}
+      </div>
+      <div className="navbar">
+        <div className="nav___style">
+          <ui className="nav___image">
+            <img src={Logo} alt="logoIMG" />
+          </ui>
+          <ui className="nav___links">
+            {navInfo.map((item, index) => (
+              <li key={index}>
+                <a href={item.url}>
+                  <p>{item.navName}</p>
+                </a>
+              </li>
+            ))}
+          </ui>
+          <ui className="nav___button">
+            <Button name="Sign In" type="btn___normal" />
+            <Button name="Sing up" type="btn___border" />
+          </ui>
+        </div>
+      </div>
     </nav>
   );
 }
